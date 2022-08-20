b43533334f7dfff32e82433e3b7aef09e20497f6
commandsean <105042417+commandsean@users.noreply.github.com>
2022-08-21 00:34:46 +0900

    Update subset.js

--- subset/subset.js
+++ subset/subset.js
@@ -1,8 +1,18 @@
 window.onload = function () {
   var subset = document.getElementById('subset');
   subset.addEventListener('click', function(){
-    const uniSet = prompt("집합을 띄어쓰기 한칸을 기준으로 입력해주세요").split(" ")
-    alert(uniSet)
+    const result = [[]]
+    var uniSet = []
+    uniSet.push(prompt("집합을 띄어쓰기 한칸을 기준으로 입력해주세요").split(" "))
+    for (let i = 0; i < uniSet.length + 1; i++){
+      for (let j = 0; j < result.length; j++){
+        result.push(uniSet[j])
+        
+      }
+    }
+    
+    alert(result)
+    
 
 
   })
