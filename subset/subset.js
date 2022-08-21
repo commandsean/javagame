639c765c69222bc3ee3d9c41ad6d8310b3529572
commandsean <105042417+commandsean@users.noreply.github.com>
2022-08-22 00:03:28 +0900

    자바로 부분집합 구하는 코드 만들기

--- subset/subset.js
+++ subset/subset.js
@@ -2,18 +2,16 @@ window.onload = function () {
   var subset = document.getElementById('subset');
   subset.addEventListener('click', function(){
     const result = [[]]
-    var uniSet = []
-    uniSet.push(prompt("집합을 띄어쓰기 한칸을 기준으로 입력해주세요").split(" "))
-    for (let i = 0; i < uniSet.length + 1; i++){
-      for (let j = 0; j < result.length; j++){
-        result.push(uniSet[j])
-        
+    const uniset_come = prompt("집합을 띄어쓰기 한칸을 기준으로 입력해주세요")
+    const front_result = [ "[]" ]
+    const uniSet = uniset_come.split(" ")
+    for (const i of uniSet){
+      for (const j in result){ 
+        result.push(result[j] + [i] + ", ")
+        front_result.push("[" + result[j] + [i] + "]") 
       }
     }
-    
-    alert(result)
-    
-
-
+    alert(front_result)
+    alert("부분집합의 개수: " + result.length)
   })
 }
